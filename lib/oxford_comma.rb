b2e0891cd0d3d5976334e3e191b846c87d8ee217
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-26 03:02:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,8 @@
 def oxford_comma(array)
-  new_array = array
-  if array.length >= 3
-    array.pop
-    new_array << "and #{array[-1]}"
-
-    array.join(', ')
-  elsif array.length == 2
+  if array.length >= 2
     array.join(' and ')
+  elsif array.length > 2
+    array[-1].insert(0, 'and')
   else
     array.join
   end
