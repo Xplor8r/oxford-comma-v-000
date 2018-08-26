06433976aede64947b5dcd64a4de3d2160fa956d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-26 02:50:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length >= 3
-    array.pop
-    array = array << "and #{array.push}"
+    array.last
+    array = array << "and #{}"
     array.join(', ')
   elsif array.length == 2
     array.join(' and ')
