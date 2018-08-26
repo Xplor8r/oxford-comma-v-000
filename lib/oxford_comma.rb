31ca5511a9ad1fc8c0580d77a56e78eaf27798fb
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-26 02:55:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length >= 3
     array.pop
-    array << "and starfruit"
+    array << "and #{array[-1]}"
     array.join(', ')
   elsif array.length == 2
     array.join(' and ')
