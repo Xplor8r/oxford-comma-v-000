72f2efda8b54d8683cd737ad3e83195b52189bd3
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-26 02:41:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length >= 3
     array.join(', ')
-    array.push("and #{array.last}")
+    array.push(and "#{array.last}")
   elsif array.length == 2
     array.join(' and ')
   else
